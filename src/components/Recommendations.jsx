9521a9a957fe367a5860a708854d752cddedfa0b
eburzlaff <du.di.99@hotmail.com>
2023-09-02 17:49:16 -0500

    Finishing similar and recommendation sections

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import ContentWrapper from './ContentWrapper';
+import { fetchDataFromApi } from '../api/tmdb';
+import Carousel from './carousel/Carousel';
+import { useParams } from 'react-router-dom';
 
 function Recommendations(props) {
+
+    const { mediaType, id } = useParams();
+    const [recommendations, setRecommendations] = useState();
+
+    useEffect(() => {
+      getRecommendations();
+    }, [])
+
+    const getRecommendations = async () => {
+        try {
+            const data = await fetchDataFromApi(`/${mediaType}/${id}/recommendations`);
+            setRecommendations(data);
+            
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
+    }
+    
     return (
-        <div>
-            Recomendations
+        <div className='mb-20'>
+            <div className='flex justify-between mb-5'>
+                <h2 className='text-white text-2xl font-medium'>Recommendations</h2>
+            </div>
+            <Carousel mediaType={mediaType === "movie" ? "movie" : "tv"} data={recommendations}/>
         </div>
     );
 }
--- src/components/Similar.jsx
+++ src/components/Similar.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import ContentWrapper from './ContentWrapper';
+import { fetchDataFromApi } from '../api/tmdb';
+import Carousel from './carousel/Carousel';
+import { useParams } from 'react-router-dom';
 
 function Similar(props) {
+
+    const { mediaType, id } = useParams();
+    const [similar, setSimilar] = useState();
+
+    const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
+
+    useEffect(() => {
+      getSimilar();
+    }, [])
+
+    const getSimilar = async () => {
+        try {
+            const data = await fetchDataFromApi(`/${mediaType}/${id}/similar`);
+            setSimilar(data);
+            
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
+    }
+    
     return (
-        <div>
-            Similar
+        <div className='mb-20'>
+            <div className='flex justify-between mb-5'>
+                <h2 className='text-white text-2xl font-medium'>{title}</h2>
+            </div>
+            <Carousel mediaType={mediaType === "movie" ? "movie" : "tv"} data={similar}/>
         </div>
     );
 }
